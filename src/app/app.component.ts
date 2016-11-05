d118e385a4b3f5d22accf3d035d380aeba278829
Lionell Briones <llenoil@gmail.com>
2016-11-06 01:35:58 +0800

    Added Firebase List operations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,43 @@ import { AngularFire, FirebaseListObservable, FirebaseObjectObservable } from 'a
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  courses: FirebaseListObservable<any>;
   title: string = 'Angular Fire 2';
 
-  constructor(af: AngularFire) {
-    this.courses = af.database.list('courses');
+  courses$: FirebaseListObservable<any>;
+  lesson$: FirebaseObjectObservable<any>;
 
-    this.courses.subscribe(console.log);
+  lastCourse: any;
+
+  constructor(af: AngularFire) {
+    this.courses$ = af.database.list('courses');
+
+    this.courses$.subscribe(
+      courses => {
+        console.log(courses);
+        this.lastCourse = courses[courses.length-1]
+      }
+    )
+
+    this.lesson$ = af.database.object('lessons/-KVLg-pSEo-rzpB-zbG1');
+
+    this.lesson$.subscribe(console.log);
+  }
+
+  listPush() {
+    this.courses$.push({description: 'TEST NEW COURSE'})
+      .then(
+        message => console.log('List Push Done', message),
+        console.error
+      );
+  }
+
+  listRemove() {
+    this.courses$.remove(this.lastCourse);
+  }
+
+  listUpdate() {
+    this.courses$.update(this.lastCourse, {
+      description: 'Modified'
+    })
   }
 }
