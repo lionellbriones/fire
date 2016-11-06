d204d273d554f7a944467037eda846d25042dedd
Lionell Briones <llenoil@gmail.com>
2016-11-06 15:06:05 +0800

    Added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AngularFireModule } from 'angularfire2';
 import { firebaseConfig } from '../environments/firebase.config';
+import { RouterModule } from '@angular/router';
 
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/do';
+
+import { routerConfig } from './router.config';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -20,6 +23,7 @@ import { LessonsListComponent } from './lessons-list/lessons-list.component';
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(routerConfig),
     AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [LessonsService],
