873bd6ad9a45cb750a834e4af3c57003774908d8
Lionell Briones <llenoil@gmail.com>
2016-11-08 09:01:55 +0800

    Navigate between lessons

--- src/app/lesson-detail/lesson-detail.component.ts
+++ src/app/lesson-detail/lesson-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
+import * as _ from 'lodash';
 
 import { Lesson } from '../shared/model/lesson';
 import { LessonsService } from '../shared/model/lessons.service';
@@ -13,16 +14,31 @@ export class LessonDetailComponent implements OnInit {
   lesson: Lesson;
 
   constructor(
+    private router: Router,
     private route: ActivatedRoute,
     private lessonService: LessonsService
   ) { }
 
   ngOnInit() {
-    const lessonUrl = this.route.snapshot.params['id'];
+    this.route.params.switchMap(params => {
+      const lessonUrl = params['id'];
 
-    const lesson$ = this.lessonService.findLessonByUrl(lessonUrl);
+      return this.lessonService.findLessonByUrl(lessonUrl);
+    }).subscribe(lesson => this.lesson = lesson);
+  }
 
-    lesson$.do(console.log).subscribe(lesson => this.lesson = lesson);
+  next() {
+    this.lessonService.loadNextLesson(this.lesson.courseId, this.lesson.$key)
+      .subscribe(this.navigateToLesson.bind(this));
+  }
+
+  previous() {
+    this.lessonService.loadPreviousLesson(this.lesson.courseId, this.lesson.$key)
+      .subscribe(this.navigateToLesson.bind(this));
+  }
+
+  navigateToLesson(lesson: Lesson){
+    this.router.navigate(['lessons', lesson.url]);
   }
 
 }
--- src/app/shared/model/lessons.service.ts
+++ src/app/shared/model/lessons.service.ts
@@ -1,27 +1,52 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Rx';
-import { AngularFire } from 'angularfire2';
+import { AngularFireDatabase } from 'angularfire2';
 
 import { Lesson } from './lesson';
 
 @Injectable()
 export class LessonsService {
 
-  constructor(private af: AngularFire) { }
+  constructor(private db: AngularFireDatabase) { }
 
   findAllLessons(): Observable<Lesson[]> {
-    return this.af.database.list('lessons')
+    return this.db.list('lessons')
       .map(Lesson.fromJsonList);
   }
 
   findLessonByUrl(lessonUrl: string): Observable<Lesson>{
-    console.log(lessonUrl);
-    return this.af.database.list('lessons', {
+    return this.db.list('lessons', {
       query: {
         orderByChild: 'url',
         equalTo: lessonUrl
       }
     }).map(results => Lesson.fromJson(results[0]));
+  }
+
+  loadNextLesson(courseId: string, key: string): Observable<Lesson>{
+     return this.db.list(`lessonsPerCourse/${courseId}`, {
+      query: {
+        orderByKey: true,
+        startAt: key,
+        limitToFirst: 2
+      }
+    })
+    .map(results => results[1].$key)
+    .switchMap(lessonId => this.db.object(`lessons/${lessonId}`))
+    .map(Lesson.fromJson);
+  }
+
+  loadPreviousLesson(courseId: string, key: string): Observable<Lesson>{
+     return this.db.list(`lessonsPerCourse/${courseId}`, {
+      query: {
+        orderByKey: true,
+        endAt: key,
+        limitToFirst: 2
+      }
+    })
+    .map(results => results[0].$key)
+    .switchMap(lessonId => this.db.object(`lessons/${lessonId}`))
+    .map(Lesson.fromJson);
   }
 
 }
